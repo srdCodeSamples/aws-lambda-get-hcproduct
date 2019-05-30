6c147466cbdd25ed4d958e56070f6ca9c6354573
Slav Donchev <slav.donchev@gmail.com>
2019-05-30 14:16:26 +0300

    add main module

---
+++ terraform/input.tf
@@ -0,0 +1,27 @@
+variable "api_name" {
+  type = "string"
+  description = "name of the API gateway"
+}
+
+variable "api_description" {
+  type    = "string"
+  default = ""
+  description = "description of the API gateway"
+}
+
+variable "function_name" {
+  type    = "string"
+  default = "hc-get-product"
+  description = "name of the lambda function"
+}
+
+variable "function_code_file" {
+  type = "string"
+  description = "path to the file containig the labda. On consecutive applies the function will be re-deployed if the file is not the same (even if contetns are not changed)."
+}
+
+variable "aws_region" {
+    type = "string"
+    default = ""
+    description = "the AWS region which to create the resources in"
+}
---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+module "lambda_function" {
+  source = "./aws-lambda"
+  function_name = "${var.function_name}"
+  function_code_file = "${var.function_code_file}" 
+}
+
+module "api_gateway" {
+    source = "./aws-api-gateway"
+    api_name = "${var.api_name}"
+    api_description = "${var.api_description}"
+    lambda_name = "${module.lambda_function.function_name}"
+    lambda_invoke_arn = "${module.lambda_function.function_invoke_arn}"
+}
