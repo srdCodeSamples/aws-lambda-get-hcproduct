a9301971fcd128bd0ac7365e5e40a836893bf707
Slav Donchev <slav.donchev@gmail.com>
2019-05-30 15:14:35 +0300

    fix formatting

--- terraform/aws-api-getaway/aws-api-gateway-input.tf
+++ terraform/aws-api-getaway/aws-api-gateway-input.tf
@@ -1,20 +1,20 @@
 variable "api_name" {
-  type = "string"
+  type        = "string"
   description = "name of the API gateway"
 }
 
 variable "api_description" {
-  type    = "string"
-  default = ""
+  type        = "string"
+  default     = ""
   description = "description of the API gateway"
 }
 
 variable "lambda_name" {
-  type = "string"
+  type        = "string"
   description = "name of the lambda fucntion to integrate with the API gateway"
 }
 
 variable "lambda_invoke_arn" {
-  type = "string"
+  type        = "string"
   description = "the lambda function invokation arn to use for the API gatway integration"
 }
