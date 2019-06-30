e9af1c9b54615b164cae8b4e18004169a623cf4d
Slav Donchev <slav.donchev@gmail.com>
2019-06-30 15:09:48 +0300

    update api-gw code to 0.12 upgrade

--- terraform/aws-api-getaway/aws-api-gateway-download-get.tf
+++ terraform/aws-api-getaway/aws-api-gateway-download-get.tf
@@ -1,7 +1,7 @@
 resource "aws_api_gateway_method" "download-get" {
-  rest_api_id          = "${aws_api_gateway_rest_api.API.id}"
-  resource_id          = "${aws_api_gateway_resource.download.id}"
-  request_validator_id = "${aws_api_gateway_request_validator.request-parameters.id}"
+  rest_api_id          = aws_api_gateway_rest_api.API.id
+  resource_id          = aws_api_gateway_resource.download.id
+  request_validator_id = aws_api_gateway_request_validator.request-parameters.id
   http_method          = "GET"
   authorization        = "NONE"
   request_parameters = {
@@ -14,21 +14,21 @@ resource "aws_api_gateway_method" "download-get" {
 
 resource "aws_api_gateway_request_validator" "request-parameters" {
   name                        = "request-parameters-only"
-  rest_api_id                 = "${aws_api_gateway_rest_api.API.id}"
+  rest_api_id                 = aws_api_gateway_rest_api.API.id
   validate_request_body       = false
   validate_request_parameters = true
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id             = "${aws_api_gateway_rest_api.API.id}"
-  resource_id             = "${aws_api_gateway_resource.download.id}"
-  http_method             = "${aws_api_gateway_method.download-get.http_method}"
+  rest_api_id             = aws_api_gateway_rest_api.API.id
+  resource_id             = aws_api_gateway_resource.download.id
+  http_method             = aws_api_gateway_method.download-get.http_method
   integration_http_method = "POST"
   type                    = "AWS"
   timeout_milliseconds    = 29000
-  uri                     = "${var.lambda_invoke_arn}"
+  uri                     = var.lambda_invoke_arn
   request_templates = {
-    "application/json" = "${file("${path.module}/templates/input-map.json")}"
+    "application/json" = file("${path.module}/templates/input-map.json")
   }
   passthrough_behavior = "NEVER"
 }
@@ -36,7 +36,7 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = "${var.lambda_name}"
+  function_name = var.lambda_name
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
@@ -44,46 +44,45 @@ resource "aws_lambda_permission" "apigw_lambda" {
 }
 
 resource "aws_api_gateway_method_response" "download-get-301" {
-  rest_api_id         = "${aws_api_gateway_rest_api.API.id}"
-  resource_id         = "${aws_api_gateway_resource.download.id}"
-  http_method         = "${aws_api_gateway_method.download-get.http_method}"
-  status_code         = "301"
-  response_parameters = { "method.response.header.Location" = true }
+  rest_api_id = aws_api_gateway_rest_api.API.id
+  resource_id = aws_api_gateway_resource.download.id
+  http_method = aws_api_gateway_method.download-get.http_method
+  status_code = "301"
+  response_parameters = {
+    "method.response.header.Location" = true
+  }
 }
 
 resource "aws_api_gateway_method_response" "download-get-500" {
-  rest_api_id = "${aws_api_gateway_rest_api.API.id}"
-  resource_id = "${aws_api_gateway_resource.download.id}"
-  http_method = "${aws_api_gateway_method.download-get.http_method}"
+  rest_api_id = aws_api_gateway_rest_api.API.id
+  resource_id = aws_api_gateway_resource.download.id
+  http_method = aws_api_gateway_method.download-get.http_method
   status_code = "500"
 }
 
 resource "aws_api_gateway_integration_response" "download-get-301" {
-  rest_api_id = "${aws_api_gateway_rest_api.API.id}"
-  resource_id = "${aws_api_gateway_resource.download.id}"
-  http_method = "${aws_api_gateway_method.download-get.http_method}"
-  status_code = "${aws_api_gateway_method_response.download-get-301.status_code}"
+  rest_api_id = aws_api_gateway_rest_api.API.id
+  resource_id = aws_api_gateway_resource.download.id
+  http_method = aws_api_gateway_method.download-get.http_method
+  status_code = aws_api_gateway_method_response.download-get-301.status_code
   response_parameters = {
     "method.response.header.Location" = "integration.response.body"
   }
   response_templates = {
-    "text/html" = "${file("${path.module}/templates/redirect.html")}"
+    "text/html" = file("${path.module}/templates/redirect.html")
   }
-  depends_on = [
-    "aws_api_gateway_integration.lambda_integration"
-  ]
+  depends_on = [aws_api_gateway_integration.lambda_integration]
 }
 
 resource "aws_api_gateway_integration_response" "download-get-500" {
-  rest_api_id       = "${aws_api_gateway_rest_api.API.id}"
-  resource_id       = "${aws_api_gateway_resource.download.id}"
-  http_method       = "${aws_api_gateway_method.download-get.http_method}"
-  status_code       = "${aws_api_gateway_method_response.download-get-500.status_code}"
+  rest_api_id       = aws_api_gateway_rest_api.API.id
+  resource_id       = aws_api_gateway_resource.download.id
+  http_method       = aws_api_gateway_method.download-get.http_method
+  status_code       = aws_api_gateway_method_response.download-get-500.status_code
   selection_pattern = "^(?!http).+$"
   response_templates = {
-    "application/json" = "${file("${path.module}/templates/output-error.json")}"
+    "application/json" = file("${path.module}/templates/output-error.json")
   }
-  depends_on = [
-    "aws_api_gateway_integration.lambda_integration"
-  ]
+  depends_on = [aws_api_gateway_integration.lambda_integration]
 }
+
